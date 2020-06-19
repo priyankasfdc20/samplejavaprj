3c0e0e272380d790ca1a597e159fa4d2eba9e9b4
priya <priyanka.gaddam@osi.ca.gov>
2020-06-18 17:25:02 -0700

    java commit

---
+++ samplejavaprj/src/test/java/com/seleniumautomation/Testjenkins.java
@@ -0,0 +1,10 @@
+package com.seleniumautomation;
+
+public class Testjenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hello jenkins::hello jenkins::hello jenkins::hello jenkins::hello jenkins::hello jenkins::hello jenkins::hello jenkins::hello jenkins::hello jenkins::hello jenkins::");
+	}
+
+}
